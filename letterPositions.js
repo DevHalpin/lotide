3ee3d2297945677a4d2d0a9af085aa0520f50c39
DevHalpin <echidnablade@hotmail.com>
2020-08-19 20:14:40 +0000

    Created letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log('🟢🟢🟢 Assertion Passed: Array 1 === Array 2');
+  } else {
+    console.log('🚫🚫🚫 Assertion Failed: Array 1 !== Array 2');
+  }
+};
+
+const letterPositions = (sentence) => {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    }  else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+}
+
+assertArraysEqual(letterPositions('hello').h,[0]);
+assertArraysEqual(letterPositions('hello').e,[1]);
+assertArraysEqual(letterPositions('hello').l,[2,3]);
+assertArraysEqual(letterPositions('hello').o,[4]);
