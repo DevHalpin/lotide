9e4fdbe90d0b9d3438e17be3a9251ce99a0fa59d
DevHalpin <echidnablade@hotmail.com>
2020-08-24 17:45:26 +0000

    Modified middleTest and tailTest to use mocha/chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-let number = [1, 2, 3, 4];
-console.log(middle([1, 2, 3, 4])); // => [2,3]
-assertArraysEqual(number, [1,2,3,4]);
-console.log(middle([1, 2, 3,4,5,6,7,8,9,10])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3])
-console.log(middle(['this','is','a','silly','test']));
+describe('#middle', () => {
+  it('should return [2,3] for [1,2,3,4]', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it('should return [5,6] for [1,2,3,4,5,6,7,8,9,10]', () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7,8,9,10]), [5,6])
+  });
+  it('should return [3] for [1,2,3,4,5]', () => {
+    assert.deepEqual(middle([1,2,3,4,5]),[3]);
+  });
+  it("should return ['a'] for ['this','is','a','silly','test']",() => {
+    assert.deepEqual(middle(['this','is','a','silly','test']),['a']);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert
 const tail = require('../tail')
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-console.log(tail([]));
-console.log(tail([1]));
+describe('#tail' , () => {
+  it("should return ['Lighthouse',['Labs'] for ['Yo Yo','Lighthouse','Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo','Lighthouse','Labs']),['Lighthouse','Labs'])
+  });
+  it('should return [] for []', () => {
+    assert.deepEqual(tail([]),[]);
+  });
+  it('should return [1] for []', () => {
+    assert.deepEqual(tail([1]),[]);
+  });
+});
